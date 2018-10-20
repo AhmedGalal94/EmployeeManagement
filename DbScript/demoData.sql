d908e09488771dacfc78fa266ac3b0da3a5c5a90
AhmedGalal <Abo.galal.9794@gmail.com>
2018-10-20 15:48:32 +0200

    Adding demo data

---
+++ DbScript/demoData.sql
@@ -0,0 +1,15 @@
+﻿INSERT [dbo].[Countries] ([Id], [Name]) VALUES (1, N'Egypt')
+INSERT [dbo].[Countries] ([Id], [Name]) VALUES (2, N'Saudi Arabia')
+
+
+INSERT [dbo].[Cities] ([Id], [Name], [CountryId]) VALUES (1, N'Cairo', 1)
+INSERT [dbo].[Cities] ([Id], [Name], [CountryId]) VALUES (2, N'Alexandria ', 1)
+INSERT [dbo].[Cities] ([Id], [Name], [CountryId]) VALUES (3, N'Riyadh', 2)
+INSERT [dbo].[Cities] ([Id], [Name], [CountryId]) VALUES (4, N'Dammam', 2)
+INSERT [dbo].[Cities] ([Id], [Name], [CountryId]) VALUES (5, N'Jeddah', 2)
+INSERT [dbo].[Cities] ([Id], [Name], [CountryId]) VALUES (7, N'Fayoum', 1)
+
+
+INSERT [dbo].[Employees] ([Id], [Name], [Email], [Salary], [CityId]) VALUES (1, N'Ahmed', N'Ahmed@email.com', 500, 4)
+INSERT [dbo].[Employees] ([Id], [Name], [Email], [Salary], [CityId]) VALUES (6, N'Mahmoud', N'Mahmoud@Email.com', 574, 1)
+INSERT [dbo].[Employees] ([Id], [Name], [Email], [Salary], [CityId]) VALUES (9, N'Mostafa', N'Mostafa@email.com', 123, 4)
